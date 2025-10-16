1b32764a2480ea9d92e7d2d14a340d02aba599b6
Phina-d <phinadiouf@gmail.com>
2025-10-16 16:58:07 +0000

    corrections

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mini-portail/",
+  base: "./", // ✅ Corrige les chemins relatifs pour GitHub Pages
 });
